61fa0648671f47fda3195544983d0a114d13eaa8
Daeun Yim <deyim33@gmail.com>
2018-06-16 18:49:05 +0900

    category all complete

--- master/controllers/8_categories_controller.js
+++ master/controllers/8_categories_controller.js
@@ -1,10 +1,40 @@
-module.exports = ()=>{
-    const express = require('express');
-    const route = express.Router();
+const db = require('../../models/index');
+const bodyParser = require('body-parser');
+const Op = db.Sequelize.Op
 
-    route.get('/', (req,res)=>{
-        
-    });
+module.exports = {
+    categoryIndex: (req,res)=>{
+        db.Category.findAll()
+        .then(categories=>{
+            res.render("8_categories/categories_index", {categories});
+        })
+    },
+    findCategory: (req,res,next)=>{
+        db.Category.findById(req.params.id)
+        .then(category=>{
+            req.category = category;
+            next();
+        })
+    },
 
-    return route;
+    // commuAdd: (req,res)=>{},
+    // commuEdit: (req,res)=>{},
+    // commuDelete: (req,res)=>{},
+    // noticeAdd: (req,res)=>{},
+    // noticeEdit: (req,res)=>{},
+    // noticeDelete: (req,res)=>{},
+    categoryAdd: (req,res)=>{
+        console.log(req.body);
+        db.Category.create(req.body)
+        .then(()=>{
+            res.redirect('/categories/');
+        })
+    },
+    categoryEdit: (req,res)=>{
+        ss;
+    },
+    categoryDelete: (req,res)=>{
+        req.category.destroy();
+        res.redirect('/categories');
+    },
 }
--- master/routes/8_categories_routes.js
+++ master/routes/8_categories_routes.js
@@ -1,10 +1,21 @@
 module.exports = ()=>{
     const express = require('express');
     const route = express.Router();
+    const controller = require('../controllers/8_categories_controller');
 
-    route.get('/', (req,res)=>{
-        
-    });
+    route.get('/', controller.categoryIndex);
+    // route.get('/communications/add', controller.commuAdd);
+    // route.get('/communications/edit', controller.commuEdit);
+    // route.get('/communications/delete', controller.commuDelete);
+
+    // route.get('/notices/add', controller.noticeAdd);
+    // route.get('/notices/edit', controller.noticeEdit);
+    // route.get('/notices/delete', controller.noticeDelete);
+
+    route.post('/add', controller.categoryAdd);
+    route.get('/edit/:id',  controller.findCategory, controller.categoryEdit);
+    route.get('/delete/:id',  controller.findCategory, controller.categoryDelete);
+   
 
     return route;
 }
